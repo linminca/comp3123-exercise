f354162afda7f7b154e26ee9ab17f8796aa59019
linminca <min-lin@hotmail.com>
2020-10-08 23:15:47 -0400

    add add.js file

--- LAB-TEST-1/question-3/add.js
+++ LAB-TEST-1/question-3/add.js
@@ -2,34 +2,75 @@ const fs = require("fs")
 
 const path = require('path'); 
 
-
-if (!fs.existsSync("Logs")) {
+__dirname = "Logs";
+if (!fs.existsSync(__dirname)) {
     // Do something
 
-    fs.mkdir(path.join(__dirname, 'Logs'), (err) => { 
-        if (err) { 
-            return console.error(err); 
-        } 
-        console.log('Directory created successfully!'); 
+    fs.mkdir(__dirname, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("Directory is created.");
     }); 
-
-    // Include process module 
+}  
+// else{
+//     console.log("Error: directory already exists");
+// }
+ 
+// Include process module 
 const process = require('process'); 
-  
+    
 // Printing current directory 
-console.log("current working directory: "
-          + process.cwd()); 
+ console.log("current working directory: "
+         + process.cwd()); 
 try { 
-      
-  // Change the directory 
-  process.chdir('../os'); 
-  console.log("working directory after "
-          + "changing: " + process.cwd()); 
+    
+// Change the directory 
+process.chdir(__dirname); 
+// console.log("working directory after "
+//         + "changing: " + process.cwd()); 
 } catch (err) { 
     
-  // Printing error if occurs 
-  console.error("error occured while "
+// Printing error if occurs 
+console.error("error occured while "
         + "changing directory: " + err); 
-} 
-    
+}; 
+
+
+for(i = 0; i< 10; i++){
+    // writeFile function with filename, content and callback function
+    fs.writeFile(`newfile${i}.txt`, 'Learn Node FS module', function (err) {
+        if (err) throw err;  
+    });
 }
+
+
+console.log("current working directory: "
+         + process.cwd()); 
+__dir2 = process.cwd();
+
+process.chdir(__dir2); 
+
+console.log("working directory after "
+         + "changing: " + process.cwd()); 
+
+         
+if (fs.existsSync(__dirname)) {
+    // Do something
+
+    
+  fs.readdir(__dirname, (err, files) => { 
+      if (err) 
+        console.log(err); 
+      else { 
+        files.forEach(file => { 
+            console.log(file); 
+        });
+      } 
+    }) 
+}
+
+
+  
+ 
+ 
